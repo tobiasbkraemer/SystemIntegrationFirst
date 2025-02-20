b1cef606337d7b47f12ec82aa9d617a4d433e547
tobiasbkraemer <tobi4533@stud.keadk>
2025-02-20 12:26:29 +0100

    20-2

---
+++ 10._Date/01._node/index.js
@@ -0,0 +1,19 @@
+console.log(new Date()); // UTC Standard: ISO 8601
+
+console.log(Date()) // Local Date
+
+console.log(Date.now()) // Unix Epoch
+
+console.log(new Date().toLocaleString()) 
+console.log(Date().toLocaleString()) 
+
+
+const date = new Date();
+
+const danishDate = new Intl.DateTimeFormat("da-dk").format(date);
+
+console.log(danishDate);
+
+const americanDate = new Intl.DateTimeFormat("en-us").format(date);
+
+console.log(americanDate);
---
+++ 10._Date/02._python/main.py
@@ -0,0 +1,8 @@
+""" import datetime
+print(datetime.datetime.now()) """
+
+from datetime import datetime
+
+print(datetime.now())
+print(datetime.now().strftime('%Y-%m-%dT&H:%M:%S'))
+
